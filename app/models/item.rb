9b7ff3aa4b5ac19bebbe73a4787d418d6ec0ee5e
mayaya6917 <maya.k03@gmail.com>
2023-12-15 12:29:31 +0900

    出品ページへの遷移、ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :sales_status
+  belongs_to :shopping_fee_status
+  belongs_to :prefecture
+  belongs_to :scheduled_delivery
+
+  validates :name, presence: true
+  validates :info, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shopping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
