ddf8f3167f2a144a3800b27b3e7f3164c5a16e61
mayaya6917 <maya.k03@gmail.com>
2023-12-30 12:51:44 +0900

    修正２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,9 +1,15 @@
-window.addEventListener('load', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const priceInput = document.getElementById("item-price");
-  priceInput.addEventListener("input", () => {
-    const addTaxDom = document.getElementById("add-tax-price");
-    addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
-    const addProfitDom = document.getElementById("profit");
-    addProfitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1 ))
- })
+
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const addTaxDom = document.getElementById("add-tax-price");
+      addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1);
+
+      const addProfitDom = document.getElementById("profit");
+      addProfitDom.innerHTML = Math.floor(priceInput.value - Math.round(priceInput.value * 0.1));
+    });
+  } else {
+    console.error("Element with id 'item-price' not found");
+  }
 });
